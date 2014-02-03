3bdb481d93f494dd2d1d40f4ff7c85be9b9e078e
Joel Brownstein <joelbrownstein@astro.utah.edu>
2014-02-03 14:10:27 -0700

    initial commit

--- redmonster/physics/__init__.py
+++ redmonster/physics/__init__.py
@@ -1,2 +1,3 @@
 # redmonster/physics/__init__.py
 # Expose modules in this package.
+from zfinder import zfinder
---
+++ redmonster/physics/zfinder.py
@@ -0,0 +1,9 @@
+class zfinder:
+
+    def __init__(self):
+        self.coeff = None
+        self.npix = None
+        
+    def set_baseline(self,coeff, npix):
+        self.coeff = coeff
+        self.npix = npix
