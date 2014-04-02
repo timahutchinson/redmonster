02a369f2b2b36f67afd601d9c18bcae8384e70ad
Tim Hutchinson <t.hutchinson@utah.edu>
2014-04-02 17:51:25 -0600

    updates

--- redmonster/datamgr/__init__.py
+++ redmonster/datamgr/__init__.py
@@ -1,3 +1,3 @@
 # redmonster/datamgr/__init__.py
 # Expose modules in this package.
-from Spec import Spec
+from spec import Spec
--- redmonster/physics/zfinder.py
+++ redmonster/physics/zfinder.py
@@ -18,27 +18,38 @@ class zfinder:
         self.templates = ssp_stuff.specs
 
     def zchi2(self, specs, ivar):
-        shape = (specs.shape[0],) + self.templates.shape[:-1]
-        zchi2arr = n.zeros(shape) # Create chi2 array of shape (# of fibers, template_parameter1, ... , template_parameterN)
-        polyarr = poly_array(self.npoly, specs.shape[1]) # Compute poly terms, noting that they will stay fixed with the data - assumes data is passed in as shape (nfibers, npix)
         num_z = self.templates.shape[-1] - specs.shape[-1] + 1 # Number of pixels to be fitted in redshift
-        bvec = n.zeros(shape=(npoly+1,num_z))
-        amat = n.zeros(shape=(npoly+1,npoly+1,num_z)) # Amat(z).f=bvec(z)
+        shape = (specs.shape[0],) + self.templates.shape[:-1] + (num_z,)
+        zchi2arr = n.zeros(shape) # Create chi2 array of shape (# of fibers, template_parameter_1, ... , template_parameter_N, # of redshifts)
+        polyarr = poly_array(self.npoly, specs.shape[1]) # Compute poly terms, noting that they will stay fixed with the data - assumes data is passed in as shape (nfibers, npix)
+        bvec = n.zeros((self.npoly+1,num_z))
+        amat = n.zeros((self.npoly+1,self.npoly+1,num_z)) # Amat(z).f=bvec(z)
+        model_mat = n.zeros((self.templates.shape[-1],self.npoly+1))
+        #model_mat[:,1:] = polyarr
         for i in range(specs.shape[0]): # Loop over fibers
-            for j in range(self.template.shape[0]): # Loop over template-ages
-                for k in range(self.template.shape[1]): # Loop over template vdisps
-                    bvec[0] = n.convolve( spec[i]/(ivar[i]**2), self.template[j,k], mode='valid' )
-                    for l in range(npoly):
-                        bvec[l+1] = n.sum( (spec[i]*polyarr[l])/(ivar[i]**2) )
-                    for m in range(npoly+1): # Fill in entries of A matrix
-                        for n in range(npoly+1):
-                            if m == 0 and n == 0: amat[m,n] = n.convolve( (self.template[j,k]*self.template[j,k]), ivar[i]**2, mode='valid' )
-                            if m == 0 and n != 0:
-                                amat[m,n] = n.convolve( self.template[j,k], polyarr[n-1] / (ivar[i]**2), mode='valid' )
-                                amat[n,m] = amat[m,n]
-                            if m != 0 and n != 0:
-                                amat[m,n] = n.sum( (polyarr[m-1]*polyarr[n-1])/(ivar[i]**2) )
-                                amat[n,m] = amat[m,n]
+            bvec[1:] = n.sum( specs[i]*polyarr*(ivar[i]**2) )
+            for j in range(self.templates.shape[0]): # Loop over template-ages
+                for k in range(self.templates.shape[1]): # Loop over template vdisps
+                    bvec[0] = n.convolve( specs[i]*(ivar[i]**2), self.templates[j,k], mode='valid' )
+                    model_mat[:,0] = self.templates[j,k]
+                    for l in range(self.npoly+1): # Fill in entries of A matrix
+                        for m in range(self.npoly+1):
+                            if l == 0 and m == 0: amat[l,m] = n.convolve( (self.templates[j,k]*self.templates[j,k]), ivar[i]**2, mode='valid' )
+                            if l == 0 and m != 0:
+                                amat[l,m] = n.convolve( self.templates[j,k], polyarr[m-1] * (ivar[i]**2), mode='valid' )
+                                amat[m,l] = amat[l,m]
+                            if l != 0 and m != 0:
+                                amat[l,m] = n.sum( (polyarr[l-1]*polyarr[m-1])*(ivar[i]**2) )
+                                amat[m,l] = amat[l,m]
                     for l in range(num_z): # Solve Amat.f = bvec at all pixel-redshifts l and computer chi2 at those redshifts
                         f = n.dot(n.linalg.inv(amat[:,:,l]),bvec[:,l])
+                        #model = n.dot(model_mat, f)
+                        #zchi2arr[i,j,k,l] = n.sum( ((specs[i]-model[l:l+specs.shape[-1]])**2)*(ivar[i]**2) )
+                        model = f[0]*self.templates[j,k]
+                        polymodel = n.zeros(specs.shape[-1])
+                        for m in range(self.npoly):
+                            polymodel += f[m+1]*polyarr[m]
+                        zchi2arr[i,j,k,l] = n.sum( ( ( specs[i] - model[l:l+specs.shape[-1]] - polymodel)**2)*(ivar[i]**2) )
+                        #import pdb; pdb.set_trace()
+        return zchi2arr
 # now use f to create model at redshift=l, compute chi^2 for that model, and plug it into zchi2arr array
