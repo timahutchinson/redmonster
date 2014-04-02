14b071402fd5eff525942a201882728660c8de68
Tim Hutchinson <t.hutchinson@utah.edu>
2014-04-02 15:27:41 -0600

    rename

--- redmonster/physics/zfinder.py
+++ redmonster/physics/zfinder.py
@@ -18,7 +18,8 @@ class zfinder:
         self.templates = ssp_stuff.specs
 
     def zchi2(self, specs, ivar):
-        zchi2 = n.zeros(shape=(n.append(specs.shape[0],self.templates.shape[:-1]))) # Create chi2 array of shape (# of fibers, template_parameter1, ... , template_parameterN)
+        shape = (specs.shape[0],) + self.templates.shape[:-1]
+        zchi2arr = n.zeros(shape) # Create chi2 array of shape (# of fibers, template_parameter1, ... , template_parameterN)
         polyarr = poly_array(self.npoly, specs.shape[1]) # Compute poly terms, noting that they will stay fixed with the data - assumes data is passed in as shape (nfibers, npix)
         num_z = self.templates.shape[-1] - specs.shape[-1] + 1 # Number of pixels to be fitted in redshift
         bvec = n.zeros(shape=(npoly+1,num_z))
@@ -40,4 +41,4 @@ class zfinder:
                                 amat[n,m] = amat[m,n]
                     for l in range(num_z): # Solve Amat.f = bvec at all pixel-redshifts l and computer chi2 at those redshifts
                         f = n.dot(n.linalg.inv(amat[:,:,l]),bvec[:,l])
-# now use f to create model at redshift=l, compute chi^2 for that model, and plug it into zchi2 array
+# now use f to create model at redshift=l, compute chi^2 for that model, and plug it into zchi2arr array
