721aa176ee21b833a4a87a644dcdbd2ef38adc5b
Tim Hutchinson <t.hutchinson@utah.edu>
2014-05-06 05:43:51 -0600

    Updates

--- redmonster/physics/Zfinder.py
+++ redmonster/physics/Zfinder.py
@@ -25,58 +25,54 @@ class Zfinder:
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
         num_z = self.templates.shape[-1] - specs.shape[-1] + 1 # Number of pixels to be fitted in redshift
         shape = (specs.shape[0],) + self.templates.shape[:-1] + (num_z,)
-        
+        fftnaxis1 = 2**13 if self.templates.shape[-1] <= 2**13 else 2**14
+
         # Create arrays for use in routine
         zchi2arr = n.zeros(shape) # Create chi2 array of shape (# of fibers, template_parameter_1, ... , template_parameter_N, # of redshifts)
         polyarr = poly_array(self.npoly, specs.shape[1]) # Compute poly terms, noting that they will stay fixed with the data - assumes data is passed in as shape (nfibers, npix)
-        bvec = n.zeros((self.npoly+1,num_z))
-        amat = n.zeros((self.npoly+1,self.npoly+1,num_z)) # Amat(z).f=bvec(z)
+        pmat = n.zeros( (self.npoly+1, self.npoly+1, fftnaxis1), dtype=float)
+        bvec = n.zeros( (self.npoly+1, fftnaxis1), dtype=float )
         
-        # Pre-compute some matrix elements
-        for i in range(self.npoly):
-            for j in range(self.npoly):
-                amat[i+1,j+1] = n.sum(polyarr[i]*polyarr[j])
+        # Pad data and SSPs to a power of 2 for faster FFTs
+        temp_pad = n.zeros(self.templates.shape[:-1] + (fftnaxis1,), dtype=float)
+        temp_pad[...,:self.templates.shape[-1]] = self.templates
+        data_pad = n.zeros(specs.shape[:-1] + (fftnaxis1,), dtype=float)
+        data_pad[...,:specs.shape[-1]] = specs
+        ivar_pad = n.zeros(ivar.shape[:-1] + (fftnaxis1,), dtype=float)
+        ivar_pad[...,:specs.shape[-1]] = ivar
+        poly_pad = n.zeros((self.npoly,fftnaxis1), dtype=float)
+        poly_pad[...,:polyarr.shape[-1]] = polyarr
         
-        # Do z computation for all fibers
-        for i in range(specs.shape[0]): # Loop over fibers
-            bvec[1:] = n.sum( specs[i]*polyarr*(ivar[i]**2) )
-            amat[1:,1:] = amat[1:,1:] * n.sum(ivar[i]**2)
-            for j in range(self.templates.shape[0]): # Loop over template_parameter1
-                for k in range(self.templates.shape[1]): # Loop over template_parameter2
-                    bvec[0] = n.convolve(specs[i]*(ivar[i]**2), self.templates[j,k], mode='valid')
-                    amat[0,0] = n.convolve( self.templates[j,k]**2, ivar[i]**2, mode='valid' )
-                    amat[1,0] = n.convolve( self.templates[j,k], polyarr[0]*(ivar[i]**2), mode='valid' )
-                    amat[2,0] = n.convolve( self.templates[j,k], polyarr[1]*(ivar[i]**2), mode='valid' )
-                    amat[3,0] = n.convolve( self.templates[j,k], polyarr[2]*(ivar[i]**2), mode='valid' )
-                    amat[0,1], amat[0,2], amat[0,3] = amat[1,0], amat[2,0], amat[3,0]
-                    for l in range(num_z): # Solve Amat.f = bvec at all pixel-redshifts l and compute chi2 at those redshifts
-                        f = n.dot(n.linalg.inv(amat[:,:,l]),bvec[:,l])
-                        model = f[0]*self.templates[j,k]
-                        polymodel = n.zeros(specs.shape[-1])
-                        for m in range(self.npoly):
-                            polymodel += f[m+1]*polyarr[m]
-                        zchi2arr[i,j,k,l] = n.sum((( specs[i] - (model[l:l+specs.shape[-1]] + polymodel) )**2)*(ivar[i]**2))
+        # Pre-compute FFTs for use in convolutions
+        t_fft = n.fft.fft(temp_pad)
+        t2_fft = n.fft.fft(temp_pad**2)
+        data_fft = n.fft.fft(data_pad * ivar_pad)
+        ivar_fft = n.fft.fft(ivar_pad)
+        poly_fft = n.zeros((ivar_pad.shape[0], self.npoly, fftnaxis1), dtype=complex)
+        for i in xrange(self.npoly):
+            poly_fft[:,i,:] = n.fft.fft(poly_pad[i] * ivar_pad)
+        
+        # Compute z for all fibers
+        for i in xrange(specs.shape[0]): # Loop over fibers
+            bvec[1:] = n.sum( poly_pad * data_pad[i] * ivar_pad[i])
+            sn_data = n.sum( (specs[i]**2)*ivar[i] )
+            for ipos in xrange(self.npoly):
+                for jpos in xrange(self.npoly): pmat[ipos+1,jpos+1] = n.sum( poly_pad[ipos] * poly_pad[jpos] * ivar_pad[i])
+            for j in xrange(self.templates.shape[0]): # Loop over template_param1
+                for k in xrange(self.templates.shape[1]): # Loop over template_param2
+                    pmat[0,0] = n.fft.ifft(t2_fft[j,k] * ivar_fft[i].conj()).real
+                    bvec[0] = n.fft.ifft(t_fft[j,k] * data_fft[i].conj()).real
+                    for ipos in xrange(self.npoly): pmat[ipos+1,0] = pmat[0,ipos+1] = n.fft.ifft(t_fft[j,k] * poly_fft[i,ipos].conj()).real
+                    for l in range(num_z): # Solve pmat.f = bvec at all pixel-redshifts l and compute chi2 at those redshifts
+                        f = n.linalg.solve(pmat[:,:,l],bvec[:,l])
+                        zchi2arr[i,j,k,l] = sn_data - n.dot(n.dot(f,pmat[:,:,l]),f)
+                        print sn_data - n.dot(n.dot(f,pmat[:,:,l]),f)
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
+        bestl = n.where(zchi2arr == n.min(zchi2arr))[3][0]
+        thisz = ((10**(specloglam[0]))/self.tempwave[bestl])-1
+        print thisz
+        return zchi2arr
         
-        
-        i = 0; j = 32; k = 2; l = 1787
-        bvec[1:] = n.sum( specs[i]*polyarr*(ivar[i]**2) )
-        amat[1:,1:] = amat[1:,1:] * n.sum(ivar[i]**2)
-        bvec[0] = n.convolve(specs[i]*(ivar[i]**2), self.templates[j,k], mode='valid')
-        amat[0,0] = n.convolve( self.templates[j,k]**2, ivar[i]**2, mode='valid' )
-        amat[1,0] = n.convolve( self.templates[j,k], polyarr[0]*(ivar[i]**2), mode='valid' )
-        amat[2,0] = n.convolve( self.templates[j,k], polyarr[1]*(ivar[i]**2), mode='valid' )
-        amat[3,0] = n.convolve( self.templates[j,k], polyarr[2]*(ivar[i]**2), mode='valid' )
-        amat[0,1], amat[0,2], amat[0,3] = amat[1,0], amat[2,0], amat[3,0]
-        f = n.dot(n.linalg.inv(amat[:,:,l]),bvec[:,l])
-        model = f[0]*self.templates[j,k]
-        polymodel = n.zeros(specs.shape[01])
-        for m in range(self.npoly):
-            polymodel += f[m+1]*polyarr[m]
-        p.plot(specs[i])
-        p.plot(model[l:l+specs.shape[-1]]+polymodel)
-        p.show()
-        return zchi2arr, amat
     
     def zchi4(self, specs, specloglam, ivar):
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
@@ -98,6 +94,7 @@ class Zfinder:
         # Compute z for all fibers
         for i in range(specs.shape[0]): # Loop over fibers
             bvec[1:] = n.sum(specs[i]*polyarr*ivar[i])
+            sn_data = n.sum( (specs[i]**2)*ivar[i] )
             for irow in range(self.npoly):
                 for icol in range(self.npoly):
                     pmat[irow+1,icol+1] = n.sum(polyarr[irow]*polyarr[icol]*ivar[i])
@@ -115,7 +112,7 @@ class Zfinder:
                         model = f[0]*self.templates[j,k]
                         polymodel = n.zeros(specs.shape[-1])
                         for m in range(self.npoly): polymodel += f[m+1]*polyarr[m]
-                        coordtrans = n.zeros((4,))
+                        #coordtrans = n.zeros((4,))
                         #zchi2arr[i,j,k,l] = n.sum((( specs[i] - (model[l:l+specs.shape[-1]] + polymodel) )**2)*(ivar[i]))
                         #zchi2arr[i,j,k,l] = n.sum( (specs[i]**2)*ivar[i]) - n.dot(n.dot(n.transpose(f),pmat[:,:,l]),f)
                         print zchi2arr[i,j,k,l]
@@ -124,23 +121,7 @@ class Zfinder:
         thisz = ((10**(specloglam[0]))/self.tempwave[bestl])-1
         print thisz
         
-        #i = 0; j = 29; k = 2; l = bestl
-        #bvec[1:] = n.sum( specs[i]*polyarr*(ivar[i]) )
-        #pmat[1:,1:] = pmat[1:,1:] * n.sum(ivar[i])
-        #bvec[0] = n.convolve(specs[i]*(ivar[i]**2), self.templates[j,k], mode='valid')
-        #pmat[0,0] = n.convolve( self.templates[j,k]**2, ivar[i]**2, mode='valid' )
-        #pmat[1,0] = n.convolve( self.templates[j,k], polyarr[0]*(ivar[i]), mode='valid' )
-        #pmat[2,0] = n.convolve( self.templates[j,k], polyarr[1]*(ivar[i]), mode='valid' )
-        #pmat[3,0] = n.convolve( self.templates[j,k], polyarr[2]*(ivar[i]), mode='valid' )
-        #pmat[0,1], pmat[0,2], pmat[0,3] = pmat[1,0], pmat[2,0], pmat[3,0]
-        #f = n.dot(n.linalg.inv(pmat[:,:,l]),bvec[:,l])
-        #model = f[0]*self.templates[j,k]
-        #polymodel = n.zeros(specs.shape[01])
-        #for m in range(self.npoly):
-        #    polymodel += f[m+1]*polyarr[m]
-        #p.plot(specs[i])
-        #p.plot(model[l:l+specs.shape[-1]]+polymodel)
-        #p.show()
+        
         
         return zchi2arr
 
