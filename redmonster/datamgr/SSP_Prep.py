3926eabdc41f250ee29594ce985fd729adae5b20
Tim Hutchinson <t.hutchinson@utah.edu>
2014-05-06 11:23:43 -0600

    Renaming again

--- redmonster/datamgr/SSP_Prep.py
+++ redmonster/datamgr/SSP_Prep.py
@@ -9,7 +9,7 @@ from time import gmtime, strftime
 # Assumes SSPs are a fits file located in $IDLSPEC2D_DIR/templates/SSPs/
 
 class SSP_Prep:
-
+    
     def __init__(self, coeff1 = .0001, velmin=None, velstep=None, nvel=None, ssp_file = 'SSP_Padova_RRLIB_Kroupa_Z0.0190.fits'):
         self.ssp_path = None
         self.hdr = None
@@ -34,7 +34,7 @@ class SSP_Prep:
                 self.rebin_ssp()
                 if self.velmin and self.velstep and nvel: self.add_velo_disp(self.velmin, self.velstep, nvel)
             else: print "%s IS NOT A VALID PATH" % self.ssp_path
-
+    
     def read_ssp(self):
         if exists(self.ssp_path):
             hdu = fits.open(self.ssp_path)
@@ -45,11 +45,11 @@ class SSP_Prep:
             logwavebounds = n.log10(self.wavebounds)
             self.coeff0 = logwavebounds[0]
             self.npix = int( n.floor( (logwavebounds[-1]-logwavebounds[0]) / self.coeff1 ) )
-
+    
     # SSPs come in units of L_sun / Hz -- convert this to erg/s/Angstrom to match BOSS spectra
     def unit_conv(self):
         self.specs = ( (3*10**18)/(self.wave**2) ) * (3.839*10**33) * self.specs
-
+    
     def rebin_ssp(self):
         self.specs = gaussian_filter1d(self.specs, sigma=(self.specs.shape[1]/float(self.npix)), order=0)
         bwb = 10**(self.coeff0 + n.arange(self.npix)*self.coeff1) # Binned wavebounds
@@ -67,12 +67,12 @@ class SSP_Prep:
                 binspecs[j,i] = ( sum(self.specs[j,parts[3,i]:parts[0,i]]) + (parts[1,i]*self.specs[j,parts[0,i]]) + (parts[2,i]*self.specs[j,parts[3,i]-1]) ) / ( (parts[0,i]-parts[3,i]) + parts[1,i] + parts[2,i] )
         self.specs = binspecs * (10**-31) # Rough normalization so SSP fluxes aren't ~50 orders of magnitude larger than BOSS fluxes
         self.wave = bound2cen(bwb)
-
+    
     def add_velo_disp(self, velmin, velstep, nvel):
         velspecs = n.zeros(shape=(self.specs.shape[0], nvel, self.npix))
         for i in range(nvel): velspecs[:,i,:] = gaussian_filter1d(self.specs, sigma=(velmin + velstep*i)/69., order=0) # Divide by 69 due to d(loglam)/dpix = .0001 corresponding to dv/dpix ~ 69 km/s
         self.specs = velspecs
-
+    
     def reduce_num(self):
         newtemps = n.zeros(shape=(self.specs.shape[0]/4., self.specs.shape[1]))
         for i in range(newtemps.shape[0]):
