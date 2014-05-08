077fa9f777eb62bffed46a6c9136fa09318cf521
Tim Hutchinson <t.hutchinson@utah.edu>
2014-05-08 06:04:57 -0600

    zfitter

--- redmonster/physics/Zfitter.py
+++ redmonster/physics/Zfitter.py
@@ -1,1 +1,16 @@
 # Routine to refine redshifts found in zfinder.py
+import numpy as n
+
+class Zfitter:
+
+    def __init__(self, zchi2):
+        self.zchi2 = zchi2
+
+    def z_refine(self):
+        zminpos = n.where(self.zchi2 == n.min(self.zchi2))
+        vecpos = ()
+        for i in xrange(len(zminpos)-1):
+            vecpos += (pos[i][0],)
+        self.bestzvec = self.zchi2[vecpos]
+
+
