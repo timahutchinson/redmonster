ac784bf011002ab40f9e4b0a6e30467878f73e17
Tim Hutchinson <t.hutchinson@utah.edu>
2014-05-09 12:18:12 -0600

    Added zmin, zmax options

--- redmonster/physics/zfinder.py
+++ redmonster/physics/zfinder.py
@@ -16,8 +16,8 @@ class Zfinder:
         self.fname = fname
         self.config = config
         self.npoly = npoly if npoly else 3
-        self.zmin = zmin
-        self.zmax = zmax
+        self.zmin = float(zmin)
+        self.zmax = float(zmax)
         try: self.specdir = environ['IDLSPEC2D_DIR']
         except: self.specdir = None
         if self.config.lower() == 'ssp': self.set_SSP(npoly=npoly)
@@ -68,64 +68,24 @@ class Zfinder:
     def create_z_baseline(self, loglam0):
         self.zbase = ((10**loglam0)/self.tempwave) - 1
     
-    def zchi2(self, specs, specloglam, ivar):
-        print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
-        num_z = self.templates.shape[-1] - specs.shape[-1] + 1 # Number of pixels to be fitted in redshift
-        shape = (specs.shape[0],) + self.templates.shape[:-1] + (num_z,)
-        #fftnaxis1 = 2**13 if self.templates.shape[-1] <= 2**13 else 2**14
-
-        # Create arrays for use in routine
-        zchi2arr = n.zeros(shape) # Create chi2 array of shape (# of fibers, template_parameter_1, ... , template_parameter_N, # of redshifts)
-        polyarr = poly_array(self.npoly, specs.shape[1]) # Compute poly terms, noting that they will stay fixed with the data - assumes data is passed in as shape (nfibers, npix)
-        pmat = n.zeros( (self.npoly+1, self.npoly+1, fftnaxis1), dtype=float)
-        bvec = n.zeros( (self.npoly+1, self.fftnaxis1), dtype=float)
-        
-        # Pad data and SSPs to a power of 2 for faster FFTs
-        temp_pad = n.zeros(self.templates.shape[:-1] + (self.fftnaxis1,), dtype=float)
-        temp_pad[...,:self.templates.shape[-1]] = self.templates
-        data_pad = n.zeros(specs.shape[:-1] + (self.fftnaxis1,), dtype=float)
-        data_pad[...,:specs.shape[-1]] = specs
-        ivar_pad = n.zeros(ivar.shape[:-1] + (self.fftnaxis1,), dtype=float)
-        ivar_pad[...,:specs.shape[-1]] = ivar
-        poly_pad = n.zeros((self.npoly, self.fftnaxis1), dtype=float)
-        poly_pad[...,:polyarr.shape[-1]] = polyarr
-        
-        # Pre-compute FFTs for use in convolutions
-        t_fft = n.fft.fft(temp_pad)
-        t2_fft = n.fft.fft(temp_pad**2)
-        data_fft = n.fft.fft(data_pad * ivar_pad)
-        ivar_fft = n.fft.fft(ivar_pad)
-        poly_fft = n.zeros((ivar_pad.shape[0], self.npoly, self.fftnaxis1), dtype=complex)
-        for i in xrange(self.npoly):
-            poly_fft[:,i,:] = n.fft.fft(poly_pad[i] * ivar_pad)
-        
-        # Compute z for all fibers
+    def conv_zbounds(self):
         #import pdb; pdb.set_trace()
-        for i in xrange(specs.shape[0]): # Loop over fibers
-            #bvec[1:,:] = n.sum( poly_pad * data_pad[i] * ivar_pad[i], axis=1)
-            #import pdb; pdb.set_trace()
-            for ipos in xrange(self.npoly): bvec[ipos+1] = n.sum( poly_pad[ipos] * data_pad[i] * ivar_pad[i])
-            sn_data = n.sum( (specs[i]**2)*ivar[i] )
-            for ipos in xrange(self.npoly):
-                for jpos in xrange(self.npoly): pmat[ipos+1,jpos+1] = n.sum( poly_pad[ipos] * poly_pad[jpos] * ivar_pad[i])
-            for j in xrange(self.templates.shape[0]): # Loop over template_param1
-                for k in xrange(self.templates.shape[1]): # Loop over template_param2
-                    pmat[0,0] = n.fft.ifft(t2_fft[j,k] * ivar_fft[i].conj()).real
-                    bvec[0] = n.fft.ifft(t_fft[j,k] * data_fft[i].conj()).real
-                    for ipos in xrange(self.npoly): pmat[ipos+1,0] = pmat[0,ipos+1] = n.fft.ifft(t_fft[j,k] * poly_fft[i,ipos].conj()).real
-                    for l in range(num_z): # Solve pmat.f = bvec at all pixel-redshifts l and compute chi2 at those redshifts
-                        f = n.linalg.solve(pmat[:,:,l],bvec[:,l])
-                        zchi2arr[i,j,k,l] = sn_data - n.dot(n.dot(f,pmat[:,:,l]),f)
-        print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
-        bestl = n.where(zchi2arr == n.min(zchi2arr))[3][0]
-        thisz = ((10**(specloglam[0]))/self.tempwave[bestl])-1
-        print thisz
-        return zchi2arr
+        zmaxpix = n.where( abs((self.zbase-self.zmin)) == n.min(abs(self.zbase-self.zmin)) )[0][0]
+        zminpix = n.where( abs((self.zbase-self.zmax)) == n.min(abs(self.zbase-self.zmax)) )[0][0]
+        return zminpix, zmaxpix
 
     def zchi3(self, specs, specloglam, ivar):
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
         self.create_z_baseline(specloglam[0])
-        num_z = self.origshape[-1] - specs.shape[-1] + 1 # Number of pixels to be fitted in redshift
+        #import pdb; pdb.set_trace()
+        if (self.zmin != None) and (self.zmax != None) and (self.zmax > self.zmin):
+            bounds_set = True
+            zminpix, zmaxpix = self.conv_zbounds()
+            num_z = zmaxpix - zminpix + 1 # Number of pixels to be fitted in redshift
+            self.zbase = self.zbase[zminpix:zminpix+num_z]
+        else:
+            bounds_set = False
+            num_z = self.origshape[-1] - specs.shape[-1] + 1 # Number of pixels to be fitted in redshift
         
         # Create arrays for use in routine
         zchi2arr = n.zeros((specs.shape[0], self.templates_flat.shape[0], num_z)) # Create chi2 array of shape (# of fibers, template_parameter_1, ... , template_parameter_N, # of redshifts)
@@ -158,16 +118,24 @@ class Zfinder:
                 pmat[0,0] = n.fft.ifft(self.t2_fft[j] * ivar_fft[i].conj()).real
                 bvec[0] = n.fft.ifft(self.t_fft[j] * data_fft[i].conj()).real
                 for ipos in xrange(self.npoly): pmat[ipos+1,0] = pmat[0,ipos+1] = n.fft.ifft(self.t_fft[j] * poly_fft[i,ipos].conj()).real
-                for l in range(num_z): # Solve pmat.f = bvec at all pixel-redshifts l and compute chi2 at those redshifts
-                    f = n.linalg.solve(pmat[:,:,l],bvec[:,l])
-                    zchi2arr[i,j,l] = sn2_data - n.dot(n.dot(f,pmat[:,:,l]),f)
+                if bounds_set:
+                    for l in range(num_z):
+                        f = n.linalg.solve(pmat[:,:,l+zminpix],bvec[:,l+zminpix])
+                        zchi2arr[i,j,l] = sn2_data - n.dot(n.dot(f,pmat[:,:,l+zminpix]),f)
+                else:
+                    for l in range(num_z):
+                        f = n.linalg.solve(pmat[:,:,l],bvec[:,l])
+                        zchi2arr[i,j,l] = sn2_data - n.dot(n.dot(f,pmat[:,:,l]),f)
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()) # For timing while testing
         if self.config == 'star':
             besttemp = n.where(zchi2arr == n.min(zchi2arr))[1][0]
             print 'TYPE: ' + self.hdr['NAME%s' % besttemp]
         zchi2arr = n.reshape(zchi2arr, (specs.shape[0],) + self.origshape[:-1] + (num_z,) )
         bestl = n.where(zchi2arr == n.min(zchi2arr))[-1][0]
-        thisz = ((10**(specloglam[0]))/self.tempwave[bestl])-1
+        if bounds_set:
+            thisz = ((10**(specloglam[0]))/self.tempwave[bestl+zminpix])-1
+        else:
+            thisz = ((10**(specloglam[0]))/self.tempwave[bestl])-1
         print thisz
         return zchi2arr
 
