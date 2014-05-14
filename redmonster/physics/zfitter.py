65dd857552ea5519d7765967d0a7fbc42cac9a05
Tim Hutchinson <t.hutchinson@utah.edu>
2014-05-14 12:00:25 -0600

    added z_errors, multi-object redshift interpolation

--- redmonster/physics/zfitter.py
+++ redmonster/physics/zfitter.py
@@ -12,22 +12,33 @@ class Zfitter:
     def __init__(self, zchi2, zbase):
         self.zchi2 = zchi2
         self.zbase = zbase
+        self.best_z = n.zeros(zchi2.shape[0])
+        self.z_err = n.zeros(zchi2.shape[0])
+    #import pdb; pdb.set_trace()
 
     def z_refine(self):
-        zminpos = n.where(self.zchi2 == n.min(self.zchi2))
-        vecpos = ()
-        for i in xrange(len(zminpos)-1):
-            vecpos += (zminpos[i][0],)
-        bestzvec = self.zchi2[vecpos]
-        posinvec = zminpos[-1][0]
-        #popt, pcov = curve_fit(quad_for_fit, self.zbase[posinvec-1:posinvec+2]*1000., bestzvec[posinvec-1:posinvec+2]) # Factor of 1000 because numerical curve_fit sometimes doesn't like small numbers
-        xp = n.linspace(self.zbase[posinvec-1], self.zbase[posinvec+1], 1000)
-        #fit = quad_for_fit(xp, popt[0]*(1000.**2), popt[1]*(1000.), popt[2]) # See above comment
-        f = quadfit(self.zbase[posinvec-1:posinvec+2], bestzvec[posinvec-1:posinvec+2])
-        fit = quad_for_fit(xp, f[0], f[1], f[2])
-        #p.plot(xp, fit, color='red')
-        #p.plot(self.zbase[posinvec-1:posinvec+2], bestzvec[posinvec-1:posinvec+2], 'ko', hold=True)
-        print xp[n.where(fit == n.min(fit))[0][0]]
+        for ifiber in xrange(self.zchi2.shape[0]):
+            zminpos = n.where(self.zchi2[ifiber] == n.min(self.zchi2[ifiber]))
+            vecpos = ()
+            for i in xrange(len(zminpos)-1):
+                vecpos += (zminpos[i][0],)
+            bestzvec = self.zchi2[(ifiber,)+vecpos]
+            posinvec = zminpos[-1][0]
+            #popt, pcov = curve_fit(quad_for_fit, self.zbase[posinvec-1:posinvec+2]*1000., bestzvec[posinvec-1:posinvec+2]) # Factor of 1000 because numerical curve_fit sometimes doesn't like small numbers
+            xp = n.linspace(self.zbase[posinvec-1], self.zbase[posinvec+1], 1000)
+            #fit = quad_for_fit(xp, popt[0]*(1000.**2), popt[1]*(1000.), popt[2]) # See above comment
+            f = quadfit(self.zbase[posinvec-1:posinvec+2], bestzvec[posinvec-1:posinvec+2])
+            fit = quad_for_fit(xp, f[0], f[1], f[2])
+            #p.plot(xp, fit, color='red')
+            #p.plot(self.zbase[posinvec-1:posinvec+2], bestzvec[posinvec-1:posinvec+2], 'ko', hold=True)
+            self.best_z[ifiber] = xp[n.where(fit == n.min(fit))[0][0]]
+            self.z_err[ifiber] = self.estimate_z_err(xp, fit)
+            print self.best_z[ifiber]
+
+    def estimate_z_err(self, xp, fit):
+        fitminloc = n.where(fit == n.min(fit)) # Index of lowest chi2
+        z_err = abs(xp[fitminloc]-xp[abs(n.min(fit)+1-fit).argmin()]) # abs() of difference between z_(chi2_min) and z_(chi2_min_+1)
+        return z_err
 
 # -----------------------------------------------------------------------------
 
