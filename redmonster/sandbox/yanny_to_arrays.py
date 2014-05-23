bfacd3a714d25c8f00a8de5f94279653ce41ee12
Tim Hutchinson <t.hutchinson@utah.edu>
2014-05-23 14:21:29 -0600

    More yanny updates

---
+++ redmonster/sandbox/yanny_to_arrays.py
@@ -0,0 +1,77 @@
+import numpy as numpy
+from redmonster.sandbox import yanny as y
+
+# Read yanny file
+x = y.yanny(filename='spInspect_alltest_bolton.par.txt', np=True)
+
+# Get fibers, zpipe, zperson for each plate
+args = n.where(x['BOSSOBJECT']['plate'] == 3686)[0]
+fibers3686 = []
+zpipe3686 = []
+zperson3686 = []
+for i in args:
+    fibers3686.append( x['BOSSOBJECT'][i][2])
+    zpipe3686.append( x['BOSSOBJECT'][i][5])
+    zperson3686.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3687)[0]
+fibers3687 = []
+zpipe3687 = []
+zperson3687 = []
+for i in args:
+    fibers3687.append( x['BOSSOBJECT'][i][2])
+    zpipe3687.append( x['BOSSOBJECT'][i][5])
+    zperson3687.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3804)[0]
+fibers3804 = []
+zpipe3804 = []
+for i in args:
+    fibers3804.append( x['BOSSOBJECT'][i][2])
+    zpipe3804.append( x['BOSSOBJECT'][i][5])
+    zperson3804.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3805)[0]
+fibers3805 = []
+zpipe3805 = []
+zperson3805 = []
+for i in args:
+    fibers3805.append( x['BOSSOBJECT'][i][2])
+    zpipe3805.append( x['BOSSOBJECT'][i][5])
+    zperson3805.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3853)[0]
+fibers3853 = []
+zpipe3853 = []
+zperson3853 = []
+for i in args:
+    fibers3853.append( x['BOSSOBJECT'][i][2])
+    zpipe3853.append( x['BOSSOBJECT'][i][5])
+    zperson3853.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3855)[0]
+fibers3855 = []
+zpipe3855 = []
+zperson3855 = []
+for i in args:
+    fibers3855.append( x['BOSSOBJECT'][i][2])
+    zpipe3855.append( x['BOSSOBJECT'][i][5])
+    zperson3855.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3856)[0]
+fibers3856 = []
+zpipe3856 = []
+zperson3856 = []
+for i in args:
+    fibers3856.append( x['BOSSOBJECT'][i][2])
+    zpipe3856.append( x['BOSSOBJECT'][i][5])
+    zperson3856.append( x['BOSSOBJECT'][i][6])
+
+args = n.where(x['BOSSOBJECT']['plate'] == 3860)[0]
+fibers3860 = []
+zpipe3860 = []
+zperson3860 = []
+for i in args:
+    fibers3860.append( x['BOSSOBJECT'][i][2])
+    zpipe3860.append( x['BOSSOBJECT'][i][5])
+    zperson3860.append( x['BOSSOBJECT'][i][6])
