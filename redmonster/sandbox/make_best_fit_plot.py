eba1d971a7405044cff6878cc50845f56f0d3a94
Tim Hutchinson <t.hutchinson@utah.edu>
2014-10-09 16:50:44 -0600

    Changes to plot maker

--- redmonster/sandbox/make_best_fit_plot.py
+++ redmonster/sandbox/make_best_fit_plot.py
@@ -27,7 +27,7 @@ specs = spec.Spec(plate=plate, mjd=mjd, fiberid=fiberid)
 # Use Nao's quasars
 ztemp = zfinder.Zfinder(fname='ndArch-QSO-V003.fits', npoly=4, zmin=.5, zmax=4.0)
 
-ztemp.zchi2(specs.flux, specs.loglambda, specs.ivar, npixstep=4)
+ztemp.zchi2(specs.flux, specs.loglambda, specs.ivar, npixstep=1)
 zfit_temp = zfitter.Zfitter(ztemp.zchi2arr, ztemp.zbase)
 zfit_temp.z_refine()
 #temp_flags = misc.comb_flags(specs, ztemp, zfit_temp)
@@ -47,8 +47,32 @@ model = n.dot(pmat, f)
 # Make plot
 p.plot(10**specs.loglambda, specs.flux[0],'r', label='Data')
 p.plot(10**specs.loglambda, model, 'k', label='Model', hold=True)
-p.title('Plate %s Fiber %s, z=%.4f' % (plate, fiberid[0], zfit_temp.z[0][0]), size=16)
-p.xlabel(r'Wavelength ($\AA$)', size=14)
-p.ylabel(r'Flux ($10^{-17}$ erg s$^{-1}$cm$^{-2}$$\AA^{-1}$)', size=14)
+p.title('Plate %s Fiber %s, z=%.4f' % (plate, fiberid[0], zfit_temp.z[0][0]), size=18)
+p.xlabel(r'Wavelength ($\AA$)', size=16)
+p.ylabel(r'Flux ($10^{-17}$ erg s$^{-1}$cm$^{-2}$$\AA^{-1}$)', size=16)
 p.legend()
+
+
+
+
+from astropy.convolution import convolve, Box1DKernel
+
+smoothed = convolve(specs.flux[0], Box1DKernel(5))
+p.plot(10**specs.loglambda, smoothed,'r', label='Data')
+p.plot(10**specs.loglambda, model, 'k', label='Model', hold=True)
+p.title('Plate %s Fiber %s, z=%.4f' % (plate, fiberid[0], zfit_temp.z[0][0]), size=18)
+p.xlabel(r'Wavelength ($\AA$)', size=16)
+p.ylabel(r'Flux ($10^{-17}$ erg s$^{-1}$cm$^{-2}$$\AA^{-1}$)', size=16)
+p.legend()
+
+
+
+
+
+
+
+
+
+
+
 
