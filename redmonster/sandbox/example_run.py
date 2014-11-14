1b60f7df2802256ae032b257aff0359b53528346
Tim Hutchinson <t.hutchinson@utah.edu>
2014-11-14 14:50:58 -0700

    added redmonster.tools.__init__.py

--- redmonster/sandbox/example_run.py
+++ redmonster/sandbox/example_run.py
@@ -1,14 +1,14 @@
 from redmonster.tools import main
 
-plates = [3686]
-mjds = [55268]
+plates = [8123]
+mjds = [56931]
 fiberid = [i for i in xrange(1000)]
 templates = ['ndArch-all-CAP-grids.fits', 'ndArch-ssp_em_galaxy-v000.fits', 'ndArch-QSO-V003.fits']
 zmin = [-.005,-.01,.4]
 zmax = [.005, 1.2, 3.5]
 npoly = [4,4,4]
-npixstep = [1,1,1]
+npixstep = [1,2,4]
 dest = '/Users/boltonlab3/scratch'
 
 for i in xrange(len(plates)):
-    main.main(plate=plates[i], mjd=mjds[i], templates=templates, fiberid=fiberid, zmin=zmin, zmax=zmax, npoly=npoly, npixstep=npixstep, clobber=False)
+    main.main(plate=plates[i], mjd=mjds[i], templates=templates, fiberid=fiberid, zmin=zmin, zmax=zmax, npoly=npoly, npixstep=npixstep, dest=dest)
