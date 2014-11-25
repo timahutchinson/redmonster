35200d0d11dd2b80a70185e822bd412367410b47
Tim Hutchinson <t.hutchinson@utah.edu>
2014-11-24 18:14:39 -0700

    .

--- python/redmonster/sandbox/example_run.py
+++ python/redmonster/sandbox/example_run.py
@@ -2,7 +2,7 @@ from redmonster.application import zfind
 
 plates = [8123]
 mjds = [56931]
-fiberid = [i for i in xrange(1000)]
+fiberid = [100] #[i for i in xrange(1000)]
 templates = ['ndArch-all-CAP-grids.fits', 'ndArch-ssp_em_galaxy-v000.fits', 'ndArch-QSO-V003.fits']
 zmin = [-.005,-.01,.4]
 zmax = [.005, 1.2, 3.5]
