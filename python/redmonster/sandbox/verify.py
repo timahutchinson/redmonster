de06f9dff5c180dbdddc96cfa4b339cb555ee37d
Tim Hutchinson <t.hutchinson@utah.edu>
2015-04-27 17:23:26 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -103,11 +103,11 @@ class verify_rm:
         self.rm_type = hdu[1].data.CLASS
 
     def compare_redshifts(self,plate,visual_fibers,visual_z):
-        import pdb; pdb.set_trace()
+        vimport pdb; pdb.set_trace()
         self.read_redmonster(plate)
         for i,fiber in enumerate(visual_fibers):
             rm_ind = n.where(self.rm_fibers == fiber)[0]
-            if rm_ind != []:
+            try:
                 rm_ind = rm_ind[0]
                 self.rm_z.append(self.rm_z1[rm_ind])
                 self.vis_z.append(visual_z[i])
