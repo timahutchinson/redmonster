eeea533043b24432e844936dffa4cd32adfd0d2d
Tim Hutchinson <t.hutchinson@utah.edu>
2015-04-27 17:36:29 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -101,9 +101,8 @@ class verify_rm:
         self.rm_zerr1 = hdu[1].data.Z_ERR1
         self.rm_fibers = hdu[1].data.FIBERID + 1 # +1 here because rm fibers are 0-based and idlspec2d are 1-based
         self.rm_type = hdu[1].data.CLASS
-
+    
     def compare_redshifts(self,plate,visual_fibers,visual_z):
-        #import pdb; pdb.set_trace()
         self.read_redmonster(plate)
         for i,fiber in enumerate(visual_fibers):
             rm_ind = n.where(self.rm_fibers == fiber)[0]
@@ -111,5 +110,15 @@ class verify_rm:
                 rm_ind = rm_ind[0]
                 self.rm_z.append(self.rm_z1[rm_ind])
                 self.vis_z.append(visual_z[i])
-            else:
-                pass
+#
+#    def compare_redshifts(self,plate,visual_fibers,visual_z):
+#        #import pdb; pdb.set_trace()
+#        self.read_redmonster(plate)
+#        for i,fiber in enumerate(visual_fibers):
+#            rm_ind = n.where(self.rm_fibers == fiber)[0]
+#            try:
+#                rm_ind = rm_ind[0]
+#                self.rm_z.append(self.rm_z1[rm_ind])
+#                self.vis_z.append(visual_z[i])
+#            else:
+#                pass
