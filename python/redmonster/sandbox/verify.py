8e646d58c3942ea9bfbb6e0f463a2e5fa59ab003
Tim Hutchinson <t.hutchinson@utah.edu>
2015-04-27 17:32:49 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -107,3 +107,7 @@ class verify_rm:
         self.read_redmonster(plate)
         for i,fiber in enumerate(visual_fibers):
             rm_ind = n.where(self.rm_fibers == fiber)[0]
+            try:
+                rm_ind = rm_ind[0]
+                self.rm_z.append(self.rm_z1[rm_ind])
+                self.vis_z.append(visual_z[i])
