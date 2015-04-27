548519a864fe49a67b8956c0de6a6cee642a11c9
Tim Hutchinson <t.hutchinson@utah.edu>
2015-04-27 16:56:22 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -1,14 +1,30 @@
 import numpy as n
+from astropy.io import fits
 from os.path import join
 from os import environ
+from glob import iglob
 from redmonster.sandbox import yanny as y
 
 class verify_rm:
 
     def __init__(self):
         self.plates = [3686,3687,3804,3805,3853,3855,3856,3860]
+        self.redmonster_dir = '/uufs/chpc.utah.edu/common/home/bolton_data0/redmonster/v5_5_12/'
+        self.yanny_to_arrays()
+        self.rm_z = []
+        self.vis_z = []
+        for i,plate in enumerate(plates):
+            if i == 0: self.compare_redshifts(plate,self.fibers3686,self.zperson3686)
+            elif i == 1: self.compare_redshifts(plate,self.fibers3687,self.zperson3687)
+            elif i == 2: self.compare_redshifts(plate,self.fibers3804,self.zperson3804)
+            elif i == 3: self.compare_redshifts(plate,self.fibers3805,self.zperson3805)
+            elif i == 4: self.compare_redshifts(plate,self.fibers3853,self.zperson3853)
+            elif i == 5: self.compare_redshifts(plate,self.fibers3855,self.zperson3855)
+            elif i == 6: self.compare_redshifts(plate,self.fibers3856,self.zperson3856)
+            elif i == 7: self.compare_redshifts(plate,self.fibers3860,self.zperson3860)
 
-    def yannys_to_arrays(self):
+
+    def yanny_to_arrays(self):
         # Read yanny file
         x = y.yanny(filename='/uufs/astro.utah.edu/common/home/u0814744/boss/spInspect_alltest_bolton.par.txt', np=True)
         # Get fibers, zpipe, zperson for each plate
@@ -76,3 +92,41 @@ class verify_rm:
             self.fibers3860.append( x['BOSSOBJECT'][i][2])
             self.zpipe3860.append( x['BOSSOBJECT'][i][5])
             self.zperson3860.append( x['BOSSOBJECT'][i][6])
+
+    def read_redmonster(self,plate):
+        platepath = join( self.redmonster_dir, plate, 'v5_5_12', 'redmonster*' )
+        for path in iglob(platepath):
+            hdu = fits.open(path)
+        self.rm_z1 = hdu[1].data.Z1
+        self.rm_zerr1 = hdu[1].data.Z_ERR1
+        self.rm_fibers = hdu[1].data.Z2 + 1 # +1 here because rm fibers are 0-based and idlspec2d are 1-based
+        self.rm_type = hdu[1].data.CLASS
+
+    def compare_redshifts(self,plate,visual_fibers,visual_z):
+        self.read_redmonster(plate)
+        for i,fiber in enumerate(visual_fibers):
+            rm_ind = n.where(self.rm_fibers == fiber)[0]
+            if rm_ind != []:
+                rm_ind = rm_ind[0]
+                rm_z.append() = self.rm_z1[rm_ind]
+                vis_z.append() = visual_z[i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
