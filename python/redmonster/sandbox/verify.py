f7f9f864312aa02213031b0f1cdef3ef344bbcb7
Tim Hutchinson <t.hutchinson@utah.edu>
2015-05-15 15:24:41 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -578,8 +578,8 @@ class verify_rm:
         #p.plot(ibins,itotal,label='i')
         #p.plot(zbins,ztotal,label='z')
         print zbins
-        print ztotal
-        print n.sort(z_sn)
+        print rbins
+        print ibins
         p.legend()
         p.savefig('failure_vs_sn.pdf')
 
