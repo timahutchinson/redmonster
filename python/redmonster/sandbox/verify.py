1f57331ca1e73514655c6a689354a401a88e3b3c
Tim Hutchinson <t.hutchinson@utah.edu>
2015-05-15 14:51:43 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -574,9 +574,9 @@ class verify_rm:
         #p.plot(rbins,rhist,color='purple',label='r-band')
         #p.plot(ibins,ihist,color='blue',label='i-band')
         #p.plot(zbins,zhist,color='cyan',label='z-band')
-        p.plot(rbins,rtotal,label='r')
+        #p.plot(rbins,rtotal,label='r')
         #p.plot(ibins,itotal,label='i')
-        #p.plot(zbins,ztotal,label='z')
+        p.plot(zbins,ztotal,label='z')
         print zbins
         print ztotal
         print n.sort(z_sn)
