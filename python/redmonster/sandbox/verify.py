910daaad9bdb1c3c0ca9d522c25c544b50c6f68a
Tim Hutchinson <t.hutchinson@utah.edu>
2015-05-15 14:05:01 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -556,6 +556,9 @@ class verify_rm:
             rbins[i] = (rbinedges[i+1]+rbinedges[i])/2.
             ibins[i] = (rbinedges[i+1]+rbinedges[i])/2.
             zbins[i] = (rbinedges[i+1]+rbinedges[i])/2.
+        rhist = rhist / float(total)
+        ihist = ihist / float(total)
+        zbins = zhist / float(total)
         p.plot(rbins,rhist,color='purple',label='r-band')
         p.plot(ibins,ihist,color='blue',label='i-band')
         p.plot(zbins,zhist,color='cyan',label='z-band')
