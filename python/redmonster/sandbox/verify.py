64637b3687588c5ca528ce3f18ae766adbf25d9c
Tim Hutchinson <t.hutchinson@utah.edu>
2015-06-03 10:17:21 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -781,7 +781,7 @@ class verify_rm:
         ax2 = f.add_subplot(1,1,1)
         labels = ['0.4<z<0.5','0.5<z<0.6','0.6<z<0.7','0.7<z<0.8']
         nbins = 25
-        for j,zmin in enumerate(n.linspace(.4,.5,1)): #from (.4,.7,4)
+        for j,zmin in enumerate(n.linspace(.4,.6,2)): #from (.4,.7,4)
             #import pdb; pdb.set_trace()
             zmax = zmin + .1
             errors = n.array([])
