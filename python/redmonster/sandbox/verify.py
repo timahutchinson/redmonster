9aa6b84b33741f0d8bef10b1313b5bac22c4518c
Tim Hutchinson <t.hutchinson@utah.edu>
2015-06-05 21:17:57 -0600

    .

--- python/redmonster/sandbox/verify.py
+++ python/redmonster/sandbox/verify.py
@@ -1007,10 +1007,10 @@ class verify_rm:
         p.xlabel(r'$\log_{10} \delta$v (km s$^{-1}$)', size=16)
         p.ylabel(r'Fraction per bin in $\log_{10} \delta$v', size=16)
         p.title('r-band', size=18)
-        p.axis([.5,2.5,0,.3])
+        p.axis([.7,2.5,0,.25])
         p.legend()
         p.subplots_adjust(wspace = .35)
-        p.savefig('/uufs/astro.utah.edu/common/home/u0814744/boss/dv_vs_sn_histos2.pdf')
+        p.savefig('/uufs/astro.utah.edu/common/home/u0814744/boss/dv_vs_sn_histos.pdf')
 
 
 
