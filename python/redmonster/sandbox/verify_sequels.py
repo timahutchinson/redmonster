b8edb3da93a54764cebe464e87499c25075a2413
Tim Hutchinson <t.hutchinson@utah.edu>
2015-09-18 10:15:35 -0600

    .

--- python/redmonster/sandbox/verify_sequels.py
+++ python/redmonster/sandbox/verify_sequels.py
@@ -1767,6 +1767,7 @@ class verify_rm:
         p.plot(xdata,ydata, drawstyle='steps-mid')
         p.xlabel(r'$\chi_{null,red}^2$',size=16)
         p.ylabel('Cumulative fraction below threshold', size=16)
+        p.grid(b=True, which='major', color='black', linestyle='--')
         p.savefig('/uufs/astro.utah.edu/common/home/u0814744/boss/rchi2_null_cumul_histo.pdf')
         p.clf()
 
