2f53c7f2cd604b7f1cf66e5d67e23e3db4adfd03
Tim Hutchinson <t.hutchinson@utah.edu>
2015-09-18 10:29:55 -0600

    .

--- python/redmonster/sandbox/verify_sequels.py
+++ python/redmonster/sandbox/verify_sequels.py
@@ -1745,7 +1745,7 @@ class verify_rm:
         else:
             rchi2_nulls = self.rm_chi2_null
             rchi2_nulls = rchi2_nulls[n.where(rchi2_nulls < 10000)[0]]
-            rchi2_nulls = rchi2_nulls[n.where(rchi2_nulls < 3000)[0]]
+            rchi2_nulls = rchi2_nulls[n.where(rchi2_nulls > 3000)[0]]
         # Plot normal histogram
         hist, binedges = n.histogram(rchi2_nulls, bins=nbins)
         normhist = hist / float(rchi2_nulls.shape[0])
