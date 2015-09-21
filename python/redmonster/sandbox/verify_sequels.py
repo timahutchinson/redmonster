bb2c72cfb279d2247e3116f57004a02f3a47bb14
Tim Hutchinson <t.hutchinson@utah.edu>
2015-09-21 15:53:04 -0600

    .

--- python/redmonster/sandbox/verify_sequels.py
+++ python/redmonster/sandbox/verify_sequels.py
@@ -1818,6 +1818,7 @@ class verify_rm:
             if bins[-1] > bins2[-1]: p.axis([bins2[0],bins[-1],0,.25])
             else: p.axis([bins2[0],bins2[-1],0,.25])
         p.xlabel(r'$\chi^2-\chi_{min}^2$', size=16)
+        p.legend()
         p.savefig('/uufs/astro.utah.edu/common/home/u0814744/boss/chi2_null_less_chi2_min_histo.pdf')
         p.clf()
 
