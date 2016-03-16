a1e89a879dfb2294d077036086e36b9e72a0de37
Tim Hutchinson <t.hutchinson@utah.edu>
2016-03-15 19:56:19 -0600

    bug fix

--- python/redmonster/sandbox/verify_sequels.py
+++ python/redmonster/sandbox/verify_sequels.py
@@ -926,8 +926,6 @@ class VerifyRM:
                   'tomato','sage','cornflowerblue','sandybrown',
                   'mediumpurple','grey'
                   ]
-        current_palette = sns.color_palette('GnBu_d')
-        sns.set_style('whitegrid')
         labels = ['0.1<z<0.2','0.2<z<0.3','0.3<z<0.4','0.4<z<0.5']
         f = p.figure()
         '''
@@ -961,6 +959,8 @@ class VerifyRM:
             p.axis([.55,2,0,.4])
             '''
         ax2 = f.add_subplot(1,1,1)
+        current_palette = sns.color_palette('GnBu_d')
+        sns.set_style('whitegrid')
         labels = ['0.6<z<0.7','0.7<z<0.8','0.8<z<0.9','0.9<z<1.0']
         nbins = 25
         for j,zmin in enumerate(n.linspace(.6,.9,4)):
