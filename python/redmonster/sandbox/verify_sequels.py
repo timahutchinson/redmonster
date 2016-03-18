43db6b790e23834b43950a921bcfc43f64f79589
Tim Hutchinson <t.hutchinson@utah.edu>
2016-03-18 14:35:50 -0600

    changed background color

--- python/redmonster/sandbox/verify_sequels.py
+++ python/redmonster/sandbox/verify_sequels.py
@@ -2297,7 +2297,7 @@ class VerifyRM:
               'mediumpurple','grey'
               ]
         labels = ['0.1<z<0.2','0.2<z<0.3','0.3<z<0.4','0.4<z<0.5']
-        sns.set_style('darkgrid')
+        sns.set_style('whitegrid.')
         sns.set_palette(sns_pal)
         sns.set_context('paper')
         f = p.figure()
@@ -2450,7 +2450,7 @@ class VerifyRM:
             yfit = fit_func(xfit, popt[0], popt[1], popt[2])
             p.plot(xfit, yfit, color='mediumpurple')
             p.text(.78*(xfit[-1]-xfit[0])+xfit[0], .78*(1.1*max([max(yfit),max(normhist)])), r'$\sigma_{\mathrm{fit}}=$%.2f' % popt[1])
-            p.text(.78*(xfit[-1]-xfit[0])+xfit[0], .72*(1.1*max([max(yfit),max(normhist)])), r'$\mu_{\mathrm{fit}}=$%.2f' % popt[2])
+            p.text(.78*(xfit[-1]-xfit[0])+xfit[0], .72*(1.1*max([max(yfit),max(normhist)]))f, r'$\mu_{\mathrm{fit}}=$%.2f' % popt[2])
 
         p.xlabel(r'$(z_2-z_1)/ (\delta z_1^2+$ $\delta z_2^2)^{1/2}$')
         p.ylabel('Fraction per bin')
