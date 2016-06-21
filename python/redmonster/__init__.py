9a2ab098f8a4a614eb367d92d1b07a17ce10b7d3
Benjamin Alan Weaver <baweaver@lbl.gov>
2016-06-21 14:30:17 -0400

    add setup.py file

--- python/redmonster/__init__.py
+++ python/redmonster/__init__.py
@@ -1,1 +1,11 @@
-#blah
+"""
+redmonster
+==========
+
+"Redmonster" is a project to develop a sophisticated and flexible
+set of Python utilities for redshift measurement, physical parameter
+measurement, and classification of one-dimensional astronomical
+spectra.
+"""
+
+from ._version import __version__
